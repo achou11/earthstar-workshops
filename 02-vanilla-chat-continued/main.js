87591acc4204a6619894962229563d5915deeb58
Andrew Chou <andrewchou@fastmail.com>
2022-10-21 09:12:17 -0400

    display name

--- 02-vanilla-chat-continued/main.js
+++ 02-vanilla-chat-continued/main.js
@@ -1,14 +1,16 @@
 import * as Earthstar from "./earthstar.bundle.js";
 
+const config = getEarthstarConfig();
+
 // Generated using Earthstar.Crypto.generateShareKeypair()
-const shareSecret = "bxx7bgoesoasq276pue24t2px5yfuqookh5jfve3dhuwt4qpchqlq";
-const shareAddress =
-  "+vanilla.bicq37hcty2xu7jx4vdxjxe5dzxnzcij5cukqwgmypokd5b5padcq";
+const shareAddress = config.shareAddresses[0];
+
+const shareSecret = config.shareSecrets[shareAddress];
 
 // Generated using Earthstar.Crypto.generateAuthorKeypair()
-const authorAddress =
-  "@chou.bfmnnhvinqqyguphg3viauxi23r3tzu2qisi56peuwpcpq35jvs4a";
-const authorSecret = "bz3vbxbifwc2v75w66xulncimyr7nryftdnyehwkzqoqotp5aumaq";
+const author = config.author;
+const authorAddress = author.address;
+const authorSecret = author.secret;
 
 // Create the replica, which stores the documents and allows us to query them
 const replica = new Earthstar.Replica({
@@ -17,12 +19,18 @@ const replica = new Earthstar.Replica({
 });
 
 // Set up references to different UI for interactive parts
-const form = document.getElementById("input-form");
-const input = document.querySelector("input");
+const displayNameForm = document.getElementById("display-name-form");
+const displayNameInput = displayNameForm.querySelector(
+  "[name=display-name-input]"
+);
+
+const messageForm = document.getElementById("message-form");
+const messageInput = messageForm.querySelector("[name=message-input]");
+
 const log = document.getElementById("log");
 
 // Write messages to the replica on form submit
-form.addEventListener("submit", async (event) => {
+messageForm.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   const result = await replica.set(
@@ -32,9 +40,25 @@ form.addEventListener("submit", async (event) => {
     },
     {
       path: `/chat/~${authorAddress}/${Date.now()}`,
-      text: input.value,
+      text: messageInput.value,
     }
   );
+
+  messageInput.value = "";
+});
+
+displayNameForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const result = await replica.set(
+    { address: authorAddress, secret: authorSecret },
+    {
+      path: `/about/~${authorAddress}/displayName`,
+      text: displayNameInput.value,
+    }
+  );
+
+  displayNameInput.value = "";
 });
 
 // Load the data from the replica and write to the doc
@@ -67,9 +91,16 @@ function updateLog(docs) {
   log.innerHTML = "";
 
   docs.forEach((doc) => {
-    const message = document.createElement("li");
+    const displayName = cache.getLatestDocAtPath(
+      `/about/~${doc.author}/displayName`
+    );
 
-    message.textContent = `${doc.author}: ${doc.text}`;
+    const parsed = Earthstar.parseAuthorAddress(doc.author);
+
+    const displayedName = displayName?.text || `@${parsed.name}`;
+
+    const message = document.createElement("li");
+    message.textContent = `${displayedName}: ${doc.text}`;
 
     log.appendChild(message);
   });
@@ -78,5 +109,30 @@ function updateLog(docs) {
 // Sync with a remote replica so that we can get updates from other internet-enabled
 const peer = new Earthstar.Peer();
 peer.addReplica(replica);
+
 // The 'live' argument keeps a persistent connection that will update the replica whenever changes are detected from the remote replica
-const syncer = peer.sync("https://es-rs-squirrel.fly.dev/sync", "live");
+const syncer = peer.sync(config.replicaServerUrls[0], "live");
+
+function getEarthstarConfig() {
+  const shareAddresses = JSON.parse(
+    localStorage.getItem("earthstar:peer:shares")
+  );
+
+  const shareSecrets = JSON.parse(
+    localStorage.getItem("earthstar:peer:share-secrets")
+  );
+
+  // Generated using Earthstar.Crypto.generateAuthorKeypair()
+  const author = JSON.parse(localStorage.getItem("earthstar:peer:author"));
+
+  const replicaServerUrls = JSON.parse(
+    localStorage.getItem("earthstar:peer:replica-servers")
+  );
+
+  return {
+    shareAddresses,
+    shareSecrets,
+    author,
+    replicaServerUrls,
+  };
+}
